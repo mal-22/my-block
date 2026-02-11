b38fbed807d012b92631e6fdf5507fae738c3cd8
mal-22 <malkul97@yahoo.com>
2026-02-11 12:26:04 +1100

    update blog post

--- app.py
+++ app.py
@@ -25,5 +25,5 @@ def write():
     return render_template("write.html")
 
 if __name__ == "__main__":
-    app.run(port=8000)
-
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port, debug=True, threaded=True)

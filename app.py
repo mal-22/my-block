6903b85e5fbdcc586bc7f84d98ed4597f72dcf32
mal-22 <malkul97@yahoo.com>
2026-02-11 16:13:51 +1100

    fix error

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, Markup, abort
+from flask import Flask, render_template, request, redirect, abort
+from markupsafe import Markup
 from datetime import datetime
 import os
 import re
